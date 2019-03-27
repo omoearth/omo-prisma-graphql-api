449c66525908a0a045814b3cb14eda43288e8f3b
Samuel Andert <samuelandert@me.com>
2019-03-27 18:22:01 +0100

    restructuring folders

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <Illustration />
     <Vimeo />
     <Explain />
-    <Voting />
+    <Cities />
     <Pricing />
   </section>
 </template>
@@ -15,7 +15,7 @@ import Header from '~/components/Header'
 import Illustration from '~/components/Illustration'
 import Explain from '~/components/Explain'
 import Vimeo from '~/components/Vimeo'
-import Voting from '~/components/Voting'
+import Cities from '~/components/Cities'
 import Pricing from '~/components/Pricing'
 
 export default {
@@ -26,7 +26,7 @@ export default {
     Header,
     Explain,
     Illustration,
-    Voting,
+    Cities,
     Pricing,
     Vimeo
   }
