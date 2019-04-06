cb4b626dd2b621bef689b71c3d95959657f0c195
Samuel Andert <samuelandert@me.com>
2019-04-06 19:50:00 +0200

    locked down prisma access with secret authentication

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://localhost:4466/prisma/dev'
+    endpoint: 'http://localhost:4466/prisma/dev',
+    secret: 'viXDp4H6A3owG37pdm7ptxzqLtkhTKNsJTV3nqRLDmQYyzu6K'
 }) 
 
 export { prisma as default }
