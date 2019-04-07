7451d3d0957fc1b77e8676f5cc57ce377960b626
Samuel Andert <samuelandert@me.com>
2019-04-07 17:24:34 +0200

    locked down single user email field

--- server/src/resolvers/User.js
+++ server/src/resolvers/User.js
@@ -1,5 +1,17 @@
-const User = {
+import getUserId from "../utils/getUserId"
 
+const User = {
+    email: {
+        fragments: 'fragment userId on User { id }',
+        resolve(parent, args, { prisma, request }, info) {
+            const userId = getUserId(request)
+            if(userId && userId === parent.id) {
+                return parent.email
+            } else {
+                return null
+            }
+        }
+    }
 }
 
 export {User as default} 
