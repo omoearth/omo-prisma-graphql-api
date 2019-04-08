385cc82e04ff7a641d3a521e38dec7df6ec00bbe
Samuel Andert <samuelandert@me.com>
2019-04-08 18:44:41 +0200

    Moving JWT Token into environemnt variables

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -2,15 +2,18 @@ import jwt from 'jsonwebtoken'
 
 const getUserId = (request, requireAuth = true) => {
     const header = request.request ? request.request.headers.authorization : request.connection.context.Authorization
-    if(header){
-        const token = header.replace('Bearer ', '' )
-        const decoded = jwt.verify(token, 'secret')
-        return decoded.userId 
+
+    if (header) {
+        const token = header.replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        return decoded.userId
     }
-    if(requireAuth) {
-        throw new Error("Authentication required")
-    }
+
+    if (requireAuth) {
+        throw new Error('Authentication required')
+    } 
+    
     return null
 }
 
-export {getUserId as default}
+export { getUserId as default }
