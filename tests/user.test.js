3774b7f7ea8f23e008bab4cc74864c80225512f7
Samuel Andert <samuelandert@me.com>
2019-04-09 02:03:16 +0200

    save jest test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,26 +1,26 @@
-// import 'cross-fetch/polyfill'
-// import ApolloBoost, {gql} from 'apollo-boost'
+import 'cross-fetch/polyfill'
+import ApolloBoost, {gql} from 'apollo-boost'
 
-// const client = new ApolloBoost({
-//     uri: 'http://localhost:4000'
-// })
+const client = new ApolloBoost({
+    uri: 'http://localhost:4000'
+})
 
-// test('Should create new user', async () => {
-//     const createUser = gql`
-//         mutation {
-//             createUser(data: {
-//                 name: "Samuel Andert",
-//                 email: "samuel@andert.me",
-//                 password: "password123"
-//             }) {
-//                 token,
-//                 user {
-//                     id
-//                 }
-//             } 
-//         }
-//     `
-//     const response = await client.mutate({
-//         mutation: createUser
-//     })
-// }) 
+test('Should create new user', async () => {
+    const createUser = gql`
+        mutation {
+            createUser(data: {
+                name: "Samuel Andert",
+                email: "samuel@andert.me",
+                password: "password123"
+            }) {
+                token,
+                user {
+                    id
+                }
+            } 
+        }
+    `
+    const response = await client.mutate({
+        mutation: createUser
+    })
+}) 
