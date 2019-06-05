ea1124e9a87dd1c503b28128644fce31eda94c90
Samuel Andert <samuelandert@me.com>
2019-06-06 00:38:27 +0200

    cahnged offer seeder

--- prisma/seed/OfferSeeder.ts
+++ prisma/seed/OfferSeeder.ts
@@ -29,7 +29,7 @@ export class OfferSeeder {
         price: 99,
         priceCurrency: "€",
         businessFunction: "RENT",
-        availabilityStarts: "est. launch 2020 Q2",
+        availabilityStarts: "est. launch 2020 Q1",
         category: "mobility"
       }),
       await prisma.createOffer({
@@ -39,13 +39,13 @@ export class OfferSeeder {
         price: 199,
         priceCurrency: "€",
         businessFunction: "RENT",
-        availabilityStarts: "est. launch 2021 Q1",
+        availabilityStarts: "est. launch 2020 Q2",
         category: "mobility"
       }),
       await prisma.createOffer({
         name: "eFlat City",
         image: "city",
-        description: "500km / month",
+        description: "750km / month",
         price: 222,
         priceCurrency: "€",
         businessFunction: "RENT",
@@ -55,7 +55,7 @@ export class OfferSeeder {
       await prisma.createOffer({
         name: "eFlat InterCity",
         image: "intercity",
-        description: "500km / month",
+        description: "2500km / month",
         price: 555,
         priceCurrency: "€",
         businessFunction: "RENT",
@@ -63,10 +63,37 @@ export class OfferSeeder {
         category: "mobility"
       }),
       await prisma.createOffer({
-        name: "Voucher",
-        image: "10",
-        description: "+20%",
-        price: 10,
+        name: "15 + 15 ø",
+        image: "0",
+        description: "50 votes",
+        price: 0,
+        priceCurrency: "€",
+        businessFunction: "BUY",
+        category: "token"
+      }),
+      await prisma.createOffer({
+        name: "30 ø",
+        image: "25",
+        description: "25 votes",
+        price: 25,
+        priceCurrency: "€",
+        businessFunction: "BUY",
+        category: "token"
+      }),
+      await prisma.createOffer({
+        name: "150 ø",
+        image: "100",
+        description: "100 votes",
+        price: 100,
+        priceCurrency: "€",
+        businessFunction: "BUY",
+        category: "token"
+      }),
+      await prisma.createOffer({
+        name: "2500 ø",
+        image: "1000",
+        description: "1000 votes",
+        price: 1000,
         priceCurrency: "€",
         businessFunction: "BUY",
         category: "token"
