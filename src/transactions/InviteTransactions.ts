27696f3aa947bf07b49469b38d302ff95a41cc3a
Samuel Andert <samuelandert@me.com>
2019-06-11 00:45:32 +0200

    changed invited by

--- src/transactions/InviteTransactions.ts
+++ src/transactions/InviteTransactions.ts
@@ -4,9 +4,8 @@ import { NodeMailer } from "../messaging/email/NodeMailer";
 export class InviteTransactions {
   async createInvitation(context: Context, invite: any) {
     var invitation = await context.prisma.createInvitation(invite.invite);
-    var inviter = await context.prisma
-      .invitation({ id: context.userid })
-      .invitedBy();
+    var inviter = await context.prisma.user({ id: context.userid });
+    if (!inviter) throw Error("not authenticated");
     var city = await context.prisma.invitation({ id: invitation.id }).city();
 
     switch (invitation.type) {
